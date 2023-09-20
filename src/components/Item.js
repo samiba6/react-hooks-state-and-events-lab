3710151c2278d6e302c3910575d9b0ff4806faab
Sami <s.babouche@esi-sba.dz>
2023-09-20 10:09:00 +0100

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React from "react"
+import {useState} from "react"
+function Item ({name, category}) {
+  const [cartItem, setCartItem] = useState(false)
 
-function Item({ name, category }) {
+
+
   return (
-    <li className="">
+    <li className={cartItem ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button  className={cartItem ? "remove" : "add"} onClick={() => setCartItem(cartItem => !cartItem)}>{(cartItem && "Remove from Cart") || "Add to Cart"}</button>
     </li>
-  );
+  )
 }
 
-export default Item;
+export default Item
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Item from "./Item";
+import React from "react"
+import Item from "./Item"
+import {useState} from "react"
 
-function ShoppingList({ items }) {
+function ShoppingList ({items}) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+  const handleFilterChange = (e) => {
+    setSelectedCategory(e.target.value)
+
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange} >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +27,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default ShoppingList;
+export default ShoppingList
